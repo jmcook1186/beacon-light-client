0d607b60d0c7c6422d79a0339abaacfa03996387
Joe <jmcook1186@gmail.com>
2021-11-16 14:08:55 +0000

    use LH BeasconState type, get epoch from slot

--- src/main.rs
+++ src/main.rs
@@ -3,68 +3,101 @@ use std::fs;
 mod node_discovery;
 mod http_requests;
 mod query_node;
-mod types;
-mod build_objects;
+//mod build_objects;
 use std::mem;
 use std::option;
-use crate::types::{BeaconBlockHeader,LightClientStore, LightClientSnapshot,
-    SyncCommittee, LightClientUpdate, BeaconState};
-
-
+use eth2::types::{BeaconState, GenericResponse, MainnetEthSpec, SyncCommittee, BeaconBlockHeader, Epoch};
+use std::sync::Arc;
+use math::round;
 
 fn main(){
     
     // set basic vars and get api key from secret
     let (node_id, node_number) = node_discovery::get_random_node_id(10, 8000);
+    let state_id = "head";
     let api_key: String = fs::read_to_string(format!("/home/joe/.lighthouse/local-testnet/node_{}/validators/api-token.txt",node_number.to_string())).expect("Nope"); 
+    let endpoint_prefix: String = format!("http://localhost:{}/eth/", &node_id);
 
-    initialize(&api_key, &node_id);
-
-    // TODO:
-    // update() currently executes indiscriminately
-    // need to make this optional - is an update necessary? If not don't execute!!
-    //update(store, &api_key, &node_id);
-
+    let state: BeaconState<MainnetEthSpec> = get_state(&api_key, &state_id, &endpoint_prefix);
+    let snapshot = make_snapshot(&state);
     
     
-}
-
-
-pub fn initialize(api_key: &str, node_id: &str){
-    // initialize() builds a snapshot and store object for the most recent finalized chackpoint
-    // the initial store object has no updates in the valid_updates field. This is
-    // populated by update()
-    let state_id = "finalized".to_string();
-    let (current_state_json, current_state) = query_node::get_full_state_object(&api_key, &node_id, &state_id);
-    let current_snapshot = build_objects::make_snapshot(&current_state);
-    //let store = build_objects::initialize_store(current_snapshot);
-
-    println!("{:?}",&current_snapshot.current_sync_committee.aggregate_pubkey);
-
+    let epoch: Epoch =state.slot().epoch(32); //32 slots per epoch https://github.com/ethereum/consensus-specs/blob/dev/specs/phase0/beacon-chain.md#compute_epoch_at_slot
+    
+    println!("{:?}",epoch);
     
 }
 
 
-pub fn update(current_store: LightClientStore, api_key: &str, node_id: &str){
-    // update takes the initial store object and fills the valid_updates field
-    // by querying the head of the chain. We can keep calling update() to 
-    // refresh new_store with up to date information.
+pub fn get_state(api_key: &str, state_id: &str, endpoint_prefix: &str)->BeaconState<MainnetEthSpec>{
 
-    let state_id = "head".to_string();
-    let (new_state_json, new_state) = query_node::get_full_state_object(&api_key, &node_id, &state_id);
-    let beacon_block_body = query_node::get_block_body(&api_key, &node_id, &state_id);
-    let new_snapshot = build_objects::make_snapshot(&new_state);
-    //let update = build_objects::get_update(&new_state, &new_snapshot, &beacon_block_body);
-    
-    //let new_store = build_objects::update_store(current_store, new_snapshot, update);
+    let state_suffix: String = format!("v2/debug/beacon/states/{}", &state_id);
 
-    //return new_store
+    let endpoint = String::from(endpoint_prefix)+&state_suffix;
+    let client = reqwest::blocking::ClientBuilder::new()
+    .timeout(None)
+      .build()
+        .unwrap();
+    let endpoint = String::from(endpoint);
+    let req = client.get(endpoint).send().unwrap();
+    let resp: GenericResponse<BeaconState<MainnetEthSpec>> = req.json().unwrap();
+    let state = resp.data;
+    return state
 }
 
-// 5620
-// 5631
-// 5646
-// 5656
-// 5666
-// 5677
-// 5685
+
+pub fn make_snapshot(state: &BeaconState<MainnetEthSpec>)-> LightClientSnapshot{
+
+    let header = state.latest_block_header();
+    let current_committee = state.current_sync_committee().unwrap();
+    let next_committee = state.next_sync_committee().unwrap();
+
+
+    let snapshot = LightClientSnapshot{
+        header: header.to_owned(),
+        current_sync_committee: current_committee.clone(),
+        next_sync_committee: next_committee.clone(),
+    };
+
+    return snapshot
+}
+
+
+// pub fn get_next_sync_committee_indices(state: BeaconState<MainnetEthSpec>) -> Something:
+
+//     """
+//     Return the sync committee indices, with possible duplicates, for the next sync committee.
+//     """
+//     use math::round;
+
+//     slot = state.slot().parse::<u32>().unwrap();
+//     epoch = round::floor(slot/32,0) + 1; //32 slots per epoch https://github.com/ethereum/consensus-specs/blob/dev/specs/phase0/beacon-chain.md#compute_epoch_at_slot
+//     println!("{:?}",epoch);
+
+
+
+
+    // MAX_RANDOM_BYTE = 2**8 - 1
+    // active_validator_indices = get_active_validator_indices(state, epoch)
+    // active_validator_count = uint64(len(active_validator_indices))
+    // seed = get_seed(state, epoch, DOMAIN_SYNC_COMMITTEE)
+    // i = 0
+    // sync_committee_indices: List[ValidatorIndex] = []
+    // while len(sync_committee_indices) < SYNC_COMMITTEE_SIZE:
+    //     shuffled_index = compute_shuffled_index(uint64(i % active_validator_count), active_validator_count, seed)
+    //     candidate_index = active_validator_indices[shuffled_index]
+    //     random_byte = hash(seed + uint_to_bytes(uint64(i // 32)))[i % 32]
+    //     effective_balance = state.validators[candidate_index].effective_balance
+    //     if effective_balance * MAX_RANDOM_BYTE >= MAX_EFFECTIVE_BALANCE * random_byte:
+    //         sync_committee_indices.append(candidate_index)
+    //     i += 1
+    // return sync_committee_indices
+
+
+
+pub struct LightClientSnapshot{
+    pub header: eth2::types::BeaconBlockHeader,
+    pub current_sync_committee: Arc<eth2::types::SyncCommittee<MainnetEthSpec>>,
+    pub next_sync_committee: Arc<eth2::types::SyncCommittee<MainnetEthSpec>>,
+}
+
--- src/types.rs
+++ src/types.rs
@@ -1,119 +1,120 @@
 // STRUCTS
-
+use eth2::types::{BeaconState, GenericResponse, MainnetEthSpec, SyncCommittee, BeaconBlockHeader};
 use serde::{Deserialize, Serialize};
 
-pub struct SyncCommittee {
-    pub pubkeys: Vec<Vec<u8>>,
-    pub aggregate_pubkey: Vec<u8>,
-  }
+// pub struct SyncCommittee {
+//     pub pubkeys: Vec<Vec<u8>>,
+//     pub aggregate_pubkey: Vec<u8>,
+//   }
   
-pub struct SyncAggregate{
-    pub sync_committee_bits: Vec<u8>,
-    pub sync_committee_signature: String,
-}
+// pub struct SyncAggregate{
+//     pub sync_committee_bits: Vec<u8>,
+//     pub sync_committee_signature: String,
+// }
 
-#[derive(Clone)]
-pub struct BeaconBlockHeader{
-    pub slot: u64,
-    pub proposer_index: u64,
-    pub parent_root: Vec<u8>,
-    pub state_root: Vec<u8>,
-    pub body_root: Vec<u8>,
-}
+// #[derive(Clone)]
+// pub struct BeaconBlockHeader{
+//     pub slot: u64,
+//     pub proposer_index: u64,
+//     pub parent_root: Vec<u8>,
+//     pub state_root: Vec<u8>,
+//     pub body_root: Vec<u8>,
+// }
   
-  
+
 pub struct LightClientSnapshot{
-    pub header: BeaconBlockHeader,
-    pub current_sync_committee: SyncCommittee,
-    pub next_sync_committee: SyncCommittee
+    pub header: eth2::types::BeaconBlockHeader,
+    pub current_sync_committee: Arc<eth2::types::SyncCommittee<MainnetEthSpec>>,
+    pub next_sync_committee: Arc<eth2::types::SyncCommittee<MainnetEthSpec>>
 }
 
 
-pub struct LightClientStore{
+// pub struct LightClientStore{
 
-    pub snapshot: LightClientSnapshot,
-    pub valid_updates: Vec<LightClientUpdate>,
+//     pub snapshot: LightClientSnapshot,
+//     pub valid_updates: Vec<LightClientUpdate>,
 
-}
-impl LightClientStore{
-    pub fn add_update(&mut self, update: LightClientUpdate) ->bool{
-        self.valid_updates.push(update);
-        true
-    }
-}
-impl LightClientStore{
-    pub fn refresh_snapshot(&mut self, snapshot: LightClientSnapshot) ->bool{
-        self.snapshot = snapshot;
-        true
-    }
-}
-
-
-pub struct LightClientUpdate{
-    pub header: BeaconBlockHeader,
-    pub next_sync_committee: SyncCommittee,
-    pub next_sync_committee_branch: Vec<u8>,
-    pub finality_header: BeaconBlockHeader,
-    pub finality_branch: Vec<u8>,
-    pub sync_committee_bits: Vec<u8>,
-    pub sync_committee_signature: String,
-    pub fork_version: String,
-}
-
-pub struct Checkpoint{
-    pub epoch: u64,
-    pub root: Vec<u8>,
-}
-
-pub struct Fork{
-    pub current_version: Vec<u8>,
-    pub epoch: u64,
-    pub previous_version: Vec<u8>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Eth1Data{
-    pub deposit_root: Vec<u8>,
-    pub deposit_count: u64,
-    pub block_hash: Vec<u8>
-}
-
-pub struct BeaconState{
-
-    pub genesis_time: u64,
-    pub genesis_validators_root: Vec<u8>,
-    pub slot: u64,
-    pub fork: Fork,
-    pub latest_block_header: BeaconBlockHeader,
-    pub block_roots: Vec<u8>,
-    pub state_roots: Vec<u8>,
-    pub historical_roots: Vec<u8>,
-    pub eth1_data: Eth1Data,
-    pub eth1_data_votes: Vec<Eth1Data>,
-    pub eth1_deposit_index: u64,
-    pub validators: Vec<Validator>,
-    pub balances: Vec<u64>,
-    pub randao_mixes: Vec<Vec<u8>>,
-    pub slashings: Vec<Vec<u8>>,
-    pub previous_epoch_participation: Vec<Vec<u8>>,
-    pub current_epoch_participation: Vec<Vec<u8>>,
-    pub justification_bits: Vec<u64>,
-    pub previous_justified_checkpoint: Checkpoint,
-    pub current_justified_checkpoint: Checkpoint,
-    pub finalized_checkpoint: Checkpoint,
-    pub inactivity_scores: Vec<u64>,
-    pub current_sync_committee: SyncCommittee,
-    pub next_sync_committee: SyncCommittee,
-}
+// }
+// impl LightClientStore{
+//     pub fn add_update(&mut self, update: LightClientUpdate) ->bool{
+//         self.valid_updates.push(update);
+//         true
+//     }
+// }
+// impl LightClientStore{
+//     pub fn refresh_snapshot(&mut self, snapshot: LightClientSnapshot) ->bool{
+//         self.snapshot = snapshot;
+//         true
+//     }
+// }
 
 
-pub struct Validator{
-    pub pubkey: Vec<u8>,
-    pub withdrawal_credentials: Vec<u8>,
-    pub effective_balance: u64,
-    pub slashed: bool,
-    pub activation_eligibility_epoch: Vec<u8>,
-    pub activation_epoch: u64,
-    pub exit_epoch: u64,
-    pub withdrawable_epoch: u64,
-}
+// pub struct LightClientUpdate{
+//     pub header: eth2::types::BeaconBlockHeader,
+//     pub next_sync_committee: Arc<eth2::types::SyncCommittee<MainnetEthSpec>>,
+//     pub next_sync_committee_branch: Vec<u8>,
+//     pub finality_header: BeaconBlockHeader,
+//     pub finality_branch: Vec<u8>,
+//     pub sync_committee_bits: Vec<u8>,
+//     pub sync_committee_signature: String,
+//     pub fork_version: String,
+// }
+
+// pub struct Checkpoint{
+//     pub epoch: u64,
+//     pub root: Vec<u8>,
+// }
+
+// pub struct Fork{
+//     pub current_version: Vec<u8>,
+//     pub epoch: u64,
+//     pub previous_version: Vec<u8>,
+// }
+
+// #[derive(Serialize, Deserialize, Debug)]
+// pub struct Eth1Data{
+//     pub deposit_root: Vec<u8>,
+//     pub deposit_count: u64,
+//     pub block_hash: Vec<u8>
+// }
+
+
+// // pub struct BeaconState{
+
+// //     pub genesis_time: u64,
+// //     pub genesis_validators_root: Vec<u8>,
+// //     pub slot: u64,
+// //     pub fork: Fork,
+// //     pub latest_block_header: BeaconBlockHeader,
+// //     pub block_roots: Vec<u8>,
+// //     pub state_roots: Vec<u8>,
+// //     pub historical_roots: Vec<u8>,
+// //     pub eth1_data: Eth1Data,
+// //     pub eth1_data_votes: Vec<Eth1Data>,
+// //     pub eth1_deposit_index: u64,
+// //     pub validators: Vec<Validator>,
+// //     pub balances: Vec<u64>,
+// //     pub randao_mixes: Vec<Vec<u8>>,
+// //     pub slashings: Vec<Vec<u8>>,
+// //     pub previous_epoch_participation: Vec<Vec<u8>>,
+// //     pub current_epoch_participation: Vec<Vec<u8>>,
+// //     pub justification_bits: Vec<u64>,
+// //     pub previous_justified_checkpoint: Checkpoint,
+// //     pub current_justified_checkpoint: Checkpoint,
+// //     pub finalized_checkpoint: Checkpoint,
+// //     pub inactivity_scores: Vec<u64>,
+// //     pub current_sync_committee: SyncCommittee,
+// //     pub next_sync_committee: SyncCommittee,
+// // }
+
+
+// pub struct Validator{
+//     pub pubkey: Vec<u8>,
+//     pub withdrawal_credentials: Vec<u8>,
+//     pub effective_balance: u64,
+//     pub slashed: bool,
+//     pub activation_eligibility_epoch: Vec<u8>,
+//     pub activation_epoch: u64,
+//     pub exit_epoch: u64,
+//     pub withdrawable_epoch: u64,
+// }
