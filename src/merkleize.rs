d6671a0ad29dd7351dd98b9a0d6aeab5f4dc4b3f
Joe <jmcook1186@gmail.com>
2022-01-14 14:20:42 +0000

    add docstring explaining control flow

--- src/merkleize.rs
+++ src/merkleize.rs
@@ -4,220 +4,24 @@ use bitvec::prelude::*;
 use sha2::{Digest, Sha256};
 use std::collections::HashMap;
 
+"""
+control flow here is:assert_eq!
+
+1) calculate leaves() (returns vec of root hashes, one per leaf)
+      calls out to pad_bytes (N chunks of 32 bytes where N == power of 2)
+          calls out to pad_to_32 (if single chunk needs padding)
+          calls out to pad_to_multiple_of_32 (if multi-chunk var needs padding)
+          calls out to chunks_to_power_of_two (if N chunks is not power of 2)
+      calls out to get_hash_root() (returns root hash of single or multichunk var)
+      calls out to mix_in_length_data() (hashes root with le_bytes representation of the var length)
+2) build_tree() (returns merkle tree as Vec<Vec<String>> where vec[i+1].len() = vec[i].len()/2)
+
+calling both funcs in sequence returns a merkle tree representation of beacon_state where
+tree[6] is the state root.
+
+"""
 
 
-pub fn get_hash_root(leaf: &Vec<u8>) -> String {
-    
-    assert!(leaf.len() >= 32);
-    assert_eq!(leaf.len() % 32, 0);
-
-    if leaf.len() == 32 {
-        let mut hasher = Sha256::new();
-        hasher.update(leaf);
-        let result = hasher.finalize_reset();
-        return hex::encode(result);
-    } else {
-        // here we deal with vars that have multiple chunks
-        // by recursively hashing pairs and returning the root
-
-        let chunked_leaf: Vec<Vec<u8>> = leaf.chunks(32).map(|s| s.into()).collect();
-
-        assert!(leaf.len() > 32);
-        assert!(leaf.len() % 32 == 0);
-        assert!(chunked_leaf.len() == leaf.len() / 32);
-
-        let root: String;
-        let mut chunks = chunked_leaf.clone();
-
-        while chunks.len() != 1 {
-            // while there are multiple nodes to hash
-            let mut temp: Vec<Vec<u8>> = vec![];
-            for i in (0..chunks.len()).step_by(2) {
-                // step through nodes in pairs
-                let mut hasher = Sha256::new();
-                hasher.update(&chunks[i]);
-                hasher.update(&chunks[i + 1]);
-                temp.push(hasher.finalize_reset().to_vec());
-            }
-            chunks = temp;
-        }
-        assert!(chunks.len() == 1);
-        assert_eq!(chunks[0].len(), 32);
-
-        let root = hex::encode(&chunks[0]);
-        // 64 hex chars = 32 bytes
-        assert_eq!(root.len(), 64);
-
-        return root;
-    }
-}
-
-pub fn mix_in_length_data(root: &str, length: &usize) -> String {
-    // mix in length data
-    // we need a bytes representation (length 32) of
-    // the var length to "mix_in_length" later
-    let length_bytes = length.to_le_bytes();
-    let length_bytes = pad_to_32(&length_bytes, &length_bytes.len());
-
-    // make sure the length that is mixed in is a 32 byte vec
-    // and that the leaf is at least 32 bytes and always a multiple
-    // of 32 bytes
-    assert_eq!(length_bytes.len(), 32);
-    let mut hasher = Sha256::new();
-    hasher.update(root);
-    hasher.update(length_bytes);
-    let hash = hasher.finalize_reset().to_vec();
-
-    let root = hex::encode(hash);
-    root
-}
-
-pub fn pad_bytes(start: usize, length: usize, serialized_state: &Vec<u8>) -> Vec<u8> {
-    // start and stop idxs for vars in ssz serialized object
-    let stop = start + length;
-    let var_as_bytes = &serialized_state[start..stop];
-
-    //check lengths are consistent
-    assert!(stop - start == length);
-    assert!(
-        stop <= serialized_state.len(),
-        "stop {:?} exceeds end of ssz obj",
-        stop
-    );
-    assert_eq!(length, stop - start);
-    assert_eq!(length, var_as_bytes.len());
-
-    if length == 32 {
-        assert_eq!(var_as_bytes.len(), 32 as usize);
-        let padded_var: Vec<u8> = var_as_bytes.to_vec();
-
-        return padded_var;
-    } else if length < 32 {
-        assert!(var_as_bytes.len() < 32);
-        let padded_var: Vec<u8> = pad_to_32(var_as_bytes, &length);
-        assert_eq!(padded_var.len(), 32);
-        return padded_var;
-    } else {
-        //else length > 32
-        assert!(length > 32, "unexpected condition: length <= 32");
-        assert!(var_as_bytes.len() > 32);
-        assert_eq!(var_as_bytes.len(), length);
-
-        if length % 32 == 0 {
-            // if length > 32 and is multiple of 32
-            let n_chunks: usize = length / 32;
-
-            if n_chunks.is_power_of_two() {
-                let padded_var: Vec<u8> = var_as_bytes.to_vec();
-                assert!(padded_var.len().is_power_of_two());
-                assert!(padded_var.len() % 32 == 0);
-
-                return padded_var;
-            } else {
-                // if length > 32 and multiple of 32
-                // but N chunks not a power of 2
-
-                let padded_var: Vec<u8> = pad_chunks_to_power2(var_as_bytes);
-                assert!(padded_var.len().is_power_of_two());
-                assert!(padded_var.len() % 32 == 0);
-                return padded_var;
-            }
-        } else {
-            //length > 32 but not a multiple of 32
-            let intermediate_var = pad_to_multiple_of_32(var_as_bytes, &length);
-
-            if intermediate_var.len().is_power_of_two() {
-                return intermediate_var;
-            } else {
-                let padded_var: Vec<u8> = pad_chunks_to_power2(&intermediate_var);
-                assert!(padded_var.len().is_power_of_two());
-                assert!(padded_var.len() % 32 == 0);
-                return padded_var;
-            }
-        }
-    }
-}
-
-pub fn pad_to_32(var: &[u8], length: &usize) -> Vec<u8> {
-    // takes ssz bytes and pads with zeros to 32 byte length
-
-    let mut padded_var: Vec<u8> = vec![];
-    let n_pad = 32 - length;
-    let pad = vec![0u8; n_pad];
-
-    for i in var.iter() {
-        padded_var.push(*i);
-    }
-
-    padded_var.extend_from_slice(&pad);
-
-    assert_eq!(padded_var.len(), 32);
-
-    return padded_var;
-}
-
-pub fn pad_to_multiple_of_32(var: &[u8], length: &usize) -> Vec<u8> {
-    // for vars with >1 chunk, pads with zeros to next multiple of 32 bytes
-
-    let mut padded_var: Vec<u8> = vec![];
-    let pad = vec![0u8; 1];
-    let mut length_mut = length.clone();
-
-    // add the var bytes to the new vec
-    for i in var.iter() {
-        padded_var.push(*i);
-    }
-
-    // add 0's to the vec until the length is a multiple of 32
-    while length_mut % 32 != 0 {
-        padded_var.extend_from_slice(&pad);
-        length_mut += 1;
-    }
-
-    // make sure the length var == actual var length
-    assert_eq!(padded_var.len(), length_mut.to_owned());
-
-    return padded_var;
-}
-
-pub fn pad_chunks_to_power2(var: &[u8]) -> Vec<u8> {
-    // for vars with multiple chunks, ensures number
-    // of chunks is a power of 2
-
-    assert!(var.len() % 32 == 0);
-    let n_chunks: usize = var.len() / 32;
-
-    // if N chunks is already a power of 2
-    if n_chunks.is_power_of_two() {
-        assert_eq!(var.len() / 32 % 2, 0);
-        assert_eq!(var.len() % 32, 0);
-        return var.to_vec();
-    } else {
-        // if N chunks is not a power of 2
-        let chunks_to_add = n_chunks.next_power_of_two() - n_chunks;
-
-        assert!(chunks_to_add != 0);
-        assert!(var.len() % 32 == 0);
-
-        let mut padded_var: Vec<u8> = vec![];
-
-        let pad = vec![0u8; 32 * chunks_to_add];
-
-        for i in var.iter() {
-            padded_var.push(*i);
-        }
-
-        padded_var.extend_from_slice(&pad);
-
-        let new_n_chunks: usize = padded_var.len() / 32;
-
-        assert_eq!(padded_var.len() % 32, 0);
-        assert!(new_n_chunks.is_power_of_two());
-        assert!(padded_var.len().is_power_of_two());
-
-        return padded_var;
-    }
-}
 
 pub fn calculate_leaves(
     serialized_state: &Vec<u8>,
@@ -358,6 +162,227 @@ pub fn build_tree(leaves: Vec<String>) -> Vec<Vec<String>> {
 
     return tree;
 }
+
+
+
+pub fn get_hash_root(leaf: &Vec<u8>) -> String {
+    
+    assert!(leaf.len() >= 32);
+    assert_eq!(leaf.len() % 32, 0);
+
+    // first, if single leaf just hash it
+    if leaf.len() == 32 {
+        let mut hasher = Sha256::new();
+        hasher.update(leaf);
+        let result = hasher.finalize_reset();
+        return hex::encode(result);
+    } else {
+        // here we deal with multiple chunks
+        // by recursively hashing pairs 
+        // and returning the root
+
+        let chunked_leaf: Vec<Vec<u8>> = leaf.chunks(32).map(|s| s.into()).collect();
+        assert!(leaf.len() > 32);
+        assert!(leaf.len() % 32 == 0);
+        assert!(chunked_leaf.len() == leaf.len() / 32);
+
+        let root: String;
+        let mut chunks = chunked_leaf.clone();
+
+        // iterate through pairs of chunks
+        // creating new vec of parent nodes
+        // hash those together and repeat until 
+        // one root left
+        while chunks.len() != 1 {
+            // while there are multiple nodes to hash
+            let mut temp: Vec<Vec<u8>> = vec![];
+            for i in (0..chunks.len()).step_by(2) {
+                // step through nodes in pairs
+                let mut hasher = Sha256::new();
+                hasher.update(&chunks[i]);
+                hasher.update(&chunks[i + 1]);
+                temp.push(hasher.finalize_reset().to_vec());
+            }
+            chunks = temp;
+        }
+        assert!(chunks.len() == 1);
+        assert_eq!(chunks[0].len(), 32);
+
+        let root = hex::encode(&chunks[0]);
+        // 64 hex chars = 32 bytes
+        assert_eq!(root.len(), 64);
+
+        return root;
+    }
+}
+
+pub fn mix_in_length_data(root: &str, length: &usize) -> String {
+    // mix in length data
+    // we need a bytes representation (length 32) of
+    // the var length to "mix_in_length" later
+    let length_bytes = length.to_le_bytes();
+    let length_bytes = pad_to_32(&length_bytes, &length_bytes.len());
+
+    // make sure the length that is mixed in is a 32 byte vec
+    // and that the leaf is at least 32 bytes and always a multiple
+    // of 32 bytes
+    assert_eq!(length_bytes.len(), 32);
+    let mut hasher = Sha256::new();
+    hasher.update(root);
+    hasher.update(length_bytes);
+    let hash = hasher.finalize_reset().to_vec();
+
+    let root = hex::encode(hash);
+    root
+}
+
+pub fn pad_bytes(start: usize, length: usize, serialized_state: &Vec<u8>) -> Vec<u8> {
+    // start and stop idxs for vars in ssz serialized object
+    let stop = start + length;
+    let var_as_bytes = &serialized_state[start..stop];
+
+    //check lengths are consistent
+    assert!(stop - start == length);
+    assert!(
+        stop <= serialized_state.len(),
+        "stop {:?} exceeds end of ssz obj",
+        stop
+    );
+    assert_eq!(length, stop - start);
+    assert_eq!(length, var_as_bytes.len());
+
+    if length == 32 {
+        assert_eq!(var_as_bytes.len(), 32 as usize);
+        let padded_var: Vec<u8> = var_as_bytes.to_vec();
+        return padded_var;
+
+    } else if length < 32 {
+        assert!(var_as_bytes.len() < 32);
+        let padded_var: Vec<u8> = pad_to_32(var_as_bytes, &length);
+        assert_eq!(padded_var.len(), 32);
+        return padded_var;
+    } else {
+        //else length > 32
+        assert!(length > 32, "unexpected condition: length <= 32");
+        assert!(var_as_bytes.len() > 32);
+        assert_eq!(var_as_bytes.len(), length);
+
+        if length % 32 == 0 {
+            // if length > 32 and is multiple of 32
+            let n_chunks: usize = length / 32;
+
+            if n_chunks.is_power_of_two() {
+                let padded_var: Vec<u8> = var_as_bytes.to_vec();
+                assert!(padded_var.len().is_power_of_two());
+                assert!(padded_var.len() % 32 == 0);
+
+                return padded_var;
+            } else {
+                // if length > 32 and multiple of 32
+                // but N chunks not a power of 2
+
+                let padded_var: Vec<u8> = pad_chunks_to_power2(var_as_bytes);
+                assert!(padded_var.len().is_power_of_two());
+                assert!(padded_var.len() % 32 == 0);
+                return padded_var;
+            }
+        } else {
+            //length > 32 but not a multiple of 32
+            let intermediate_var = pad_to_multiple_of_32(var_as_bytes, &length);
+
+            if intermediate_var.len().is_power_of_two() {
+                return intermediate_var;
+            } else {
+                let padded_var: Vec<u8> = pad_chunks_to_power2(&intermediate_var);
+                assert!(padded_var.len().is_power_of_two());
+                assert!(padded_var.len() % 32 == 0);
+                return padded_var;
+            }
+        }
+    }
+}
+
+pub fn pad_to_32(var: &[u8], length: &usize) -> Vec<u8> {
+    // takes ssz bytes and pads with zeros to 32 byte length
+
+    let mut padded_var: Vec<u8> = vec![];
+    let n_pad = 32 - length;
+    let pad = vec![0u8; n_pad];
+
+    for i in var.iter() {
+        padded_var.push(*i);
+    }
+
+    padded_var.extend_from_slice(&pad);
+
+    assert_eq!(padded_var.len(), 32);
+
+    return padded_var;
+}
+
+pub fn pad_to_multiple_of_32(var: &[u8], length: &usize) -> Vec<u8> {
+    // for vars with >1 chunk, pads with zeros to next multiple of 32 bytes
+
+    let mut padded_var: Vec<u8> = vec![];
+    let pad = vec![0u8; 1];
+    let mut length_mut = length.clone();
+
+    // add the var bytes to the new vec
+    for i in var.iter() {
+        padded_var.push(*i);
+    }
+
+    // add 0's to the vec until the length is a multiple of 32
+    while length_mut % 32 != 0 {
+        padded_var.extend_from_slice(&pad);
+        length_mut += 1;
+    }
+
+    // make sure the length var == actual var length
+    assert_eq!(padded_var.len(), length_mut.to_owned());
+
+    return padded_var;
+}
+
+pub fn pad_chunks_to_power2(var: &[u8]) -> Vec<u8> {
+    // for vars with multiple chunks, ensures number
+    // of chunks is a power of 2
+
+    assert!(var.len() % 32 == 0);
+    let n_chunks: usize = var.len() / 32;
+
+    // if N chunks is already a power of 2
+    if n_chunks.is_power_of_two() {
+        assert_eq!(var.len() / 32 % 2, 0);
+        assert_eq!(var.len() % 32, 0);
+        return var.to_vec();
+    } else {
+        // if N chunks is not a power of 2
+        let chunks_to_add = n_chunks.next_power_of_two() - n_chunks;
+
+        assert!(chunks_to_add != 0);
+        assert!(var.len() % 32 == 0);
+
+        let mut padded_var: Vec<u8> = vec![];
+
+        let pad = vec![0u8; 32 * chunks_to_add];
+
+        for i in var.iter() {
+            padded_var.push(*i);
+        }
+
+        padded_var.extend_from_slice(&pad);
+
+        let new_n_chunks: usize = padded_var.len() / 32;
+
+        assert_eq!(padded_var.len() % 32, 0);
+        assert!(new_n_chunks.is_power_of_two());
+        assert!(padded_var.len().is_power_of_two());
+
+        return padded_var;
+    }
+}
+
 
 pub fn remove_cap_from_justification_bits(justification_bits: &Vec<u8>) -> Vec<u8> {
     let mut bits: BitVec = BitVec::from_bytes(&justification_bits);
